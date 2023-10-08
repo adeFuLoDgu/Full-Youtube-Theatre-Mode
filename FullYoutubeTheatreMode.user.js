ea4c11f0a377abe4077ba6a8b15ee6ea81d3f660
adeFuLoDgu <64475795+adeFuLoDgu@users.noreply.github.com>
2023-10-08 12:42:25 +0800

    Update FullYoutubeTheatreMode.user.js

--- FullYoutubeTheatreMode.user.js
+++ FullYoutubeTheatreMode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Full Youtube Theatre Mode
 // @namespace   https://github.com/adeFuLoDgu/Full-Youtube-Theatre-Mode
-// @version     0.3
+// @version     0.4
 // @description Enlarges the media player to fill the entire screen with theatre mode.
 // @author      adeFuLoDgu
 // @include     https://www.youtube.com/*
@@ -11,11 +11,6 @@
 // @grant       none
 // ==/UserScript==
 
-/**
-A simple script to fix youtube's CSS to set the containers height to 100vh - navbar height
-**/
-
-// Injects CSS into the header of the document.
 function addStyle(styleText){
 	let s = document.createElement('style');
 	s.setAttribute("id", "youtubetheater");
@@ -23,15 +18,14 @@ function addStyle(styleText){
 	document.getElementsByTagName('head')[0].appendChild(s);
 }
 
-setInterval(loadCss,1000);
-
 function loadCss(){
 	'use strict';
 	if(document.getElementById("youtubetheater")){return;}
-	// Applys css to the page to resize the media player to the entire screen.
 	addStyle(`
 		ytd-watch-flexy[theater-requested_] #full-bleed-container{
 			min-height:calc(100vh - 56px)!important;
 		}
 	`);
 };
+
+loadCss();
